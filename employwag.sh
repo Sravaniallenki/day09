25dcd7b25d8031e70a1e51a5965bc3fa5f6b4f96
sravani <allenkisravani996@gmail.com>
2020-05-01 22:03:16 +0530

    calculating part_time also

--- employwag.sh
+++ employwag.sh
@@ -1,8 +1,18 @@
 #! /bin/bash/
 random=$(( $RANDOM%2 ))
+hour_wage=20
 if [ $random -eq 1 ]
 then
-    echo "employee is present"
+   work_hours=$(( $RANDOM%8 ))
+ if [ $work_hours -eq 7 ]
+  then
+   echo "employee is present full_time"
+   echo "daily wage = "$(( 8*$hour_wage ))
+   else
+    echo "employee is present part_time"
+    echo "dialy wage = "$(( ($work_hours+1)*$hour_wage ))
+  fi
    else
    echo "employee is absent"
+   echo "daily wage = 0"
  fi
